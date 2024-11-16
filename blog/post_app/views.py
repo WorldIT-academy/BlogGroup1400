1f7be8b19a186a90bd4df90e4278f565dd448e05
Dima <you@example.com>
2024-11-16 14:32:06 +0100

    create static files and models

--- blog/post_app/views.py
+++ blog/post_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def render_all_posts(request):
-    return render(request, "post_app/all_posts.html")
+
+    all_posts = Post.objects.all()
+       
+    return render(
+        request,         
+        "post_app/all_posts.html",
+        context = {"all_posts": all_posts}
+    )
 
 def render_view_post(request):
     return render(request, "post_app/view_post.html")
